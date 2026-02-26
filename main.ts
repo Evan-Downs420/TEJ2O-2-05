33387c31938629b69713827fbd051fbcfb4dd5b3
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-26 13:11:33 -0500

    hefgr

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Evan
+ * Created on: Feb 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let temperatureInCelsius: number
+let temperatureInKelvin: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    let temperature = input.temperature()
+    temperatureInKelvin = (temperature + 273.15)
+    temperatureInKelvin = Math.round(temperature)
+
+    basic.clearScreen()
+    basic.showString("The temperature is" + (temperature) + "K")
+})
